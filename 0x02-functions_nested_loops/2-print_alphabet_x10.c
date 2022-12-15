3797b44aaf202f1b100346ac3567e4fec8578529
yourangel4 <moyinoluwa20@gmail.comm>
2022-12-15 02:39:41 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,17 @@
 
 /**
  * print_alphabet_x_10 - prints alphabet 10 times in lowercase
+ * Return: void
  */
-
 void print_alphabet_x10(void)
 {
 	int ten;
 	char la;
 
 	for (ten = 0; ten <= 9; ten++)
-	
-		for (la = 'a'; la <= 'z'; la++)
+	{
+    		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
 		_putchar('\n');
+	}
 }
