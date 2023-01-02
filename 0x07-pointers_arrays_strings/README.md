94d9b965685eb02ef0a735410296ebab2c9e3405
yourangel4 <moyinoluwa20@gmail.comm>
2023-01-02 03:32:54 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+project 0x07 even more pointers arrays and strings
+done using ubuntu webterminal
+by moyinoluwa
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned, and errno is set approp0riately
+ */
+int _putchar(char c)
+{
+	return (write(1, &C, 1));
+}
