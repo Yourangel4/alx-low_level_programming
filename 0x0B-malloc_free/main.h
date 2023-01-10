2f9ed9ab6a525dbe4a6c1cda11101de1e93a1493
yourangel4 <moyinoluwa20@gmail.comm>
2023-01-09 23:36:34 -0800

    craete array

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN _H
 #define MAIN_H
-
+#include <stsdio.h>
+#include <stdlib.h>
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
-void free_grind(int **grid, int height);
+void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 
 #endif
