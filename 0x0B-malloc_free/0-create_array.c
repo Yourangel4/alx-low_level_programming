54694b5c4b4deb43947629a18aade1b0b186bfcc
yourangel4 <moyinoluwa20@gmail.comm>
2023-01-09 23:13:16 -0800

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
  * create_array - create array of size size and assign char c
  * @size: size of array
  * @c: char to assign
- * description: create array of size size and assign char c
+ * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
  *
  */
