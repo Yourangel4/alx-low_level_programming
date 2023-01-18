538c45ff5073c47ec9af561c56ec7592f42e5975
yourangel4 <moyinoluwa20@gmail.comm>
2023-01-18 08:25:20 -0800

    dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "dog.h"
 
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
